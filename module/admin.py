6ea768913689c21e0ca004c5b2249476ec68cdad
Aysu Gafarli <aysuqafarli02@gmail.com>
2024-06-21 21:37:17 +0400

    updated

--- module/admin.py
+++ module/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(Unit)
 admin.site.register(Module)
 admin.site.register(Content)
+admin.site.register(Section)
 admin.site.register(Score)
